8b27ce80e01a34caffe54d543a34f6d713152b32
Rashidul Islam Alvi <rashidul537@gmail.com>
2024-01-21 23:24:41 +0600

    Update page.tsx
    >created landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            poppins.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+
+        <div>
+          <Button variant="secondary" size="lg">
+            Sign In
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
