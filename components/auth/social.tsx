ca50ff57d29a0d57d9bd166202a560bd77373d14
Rashidul Islam Alvi <rashidul537@gmail.com>
2024-01-22 18:05:58 +0600

    Create social.tsx

---
+++ components/auth/social.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Button } from "../ui/button";
+
+const Social = () => {
+  return (
+    <div className="flex items-center w-full gap-x-2">
+      <Button
+        size={"lg"}
+        variant={"outline"}
+        className="w-full"
+        onClick={() => {}}
+      >
+        <FcGoogle />
+      </Button>
+
+      <Button
+        size={"lg"}
+        variant={"outline"}
+        className="w-full"
+        onClick={() => {}}
+      >
+        <FaGithub />
+      </Button>
+    </div>
+  );
+};
+
+export default Social;
