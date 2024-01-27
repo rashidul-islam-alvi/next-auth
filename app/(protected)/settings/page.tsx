0f180bd0e46679a0526a7da1d83326720fd0e8f2
Rashidul Islam Alvi <rashidul537@gmail.com>
2024-01-27 23:22:17 +0600

    Update page.tsx
    added a logout button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,25 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 import React from "react";
 
 const page = async () => {
   const session = await auth();
 
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button className="border-2 px-2 py-2 border-black" type="submit">
+          Log out
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default page;
