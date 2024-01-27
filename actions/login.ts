d16c828a0e05800502b91fa88dcd878ba83c1fbc
Rashidul Islam Alvi <rashidul537@gmail.com>
2024-01-27 23:05:01 +0600

    Update login.ts
    login with next auth credentials

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,10 @@
 "use server";
 
+import { signIn } from "@/auth";
+import { DEFAULT_REDIRECT_URL } from "@/routes";
 import { LoginSchema } from "@/schemas";
+import { error } from "console";
+import { AuthError } from "next-auth";
 import * as z from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -10,5 +14,26 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_REDIRECT_URL,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials" };
+        case "AuthorizedCallbackError":
+          return { error: "Unauthorized" };
+        default:
+          return { error: "Internal server error" };
+      }
+    }
+
+    throw error;
+  }
 };
